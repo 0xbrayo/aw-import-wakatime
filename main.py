03dcf9e1dd72d04dd5b6bb61d784efce458ac5a4
brayo <vukubrian@gmail.com>
2024-07-03 19:50:17 +0300

    feat: final version

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from calendar import c
 import json
-
+import datetime
+from time import sleep
 from aw_core.models import Event
 from aw_client import ActivityWatchClient
-from more_itertools import bucket
+
+# configure the following variables to your needs
+bucket_id = "wakatime"
+filepath = "/path/to/wakatime-heartbeats.json"
+pulsetime = 300 # merges heartbeats that are within 5 minutes of each other
 
 client = ActivityWatchClient("aw-wakatime")
 client.connect()
 
-bucket_id = "wakatime"
-client.create_bucket(bucket_id) # customize this to your needs
-
-filepath = "/path/to/your/wakatime-heartbeats.json"
+client.create_bucket(bucket_id, event_type="app.editor.activity")
 
 file = open(filepath, "r")
 obj = json.load(file)
 
-for heartbeat in obj["days"][0]["heartbeats"]:
-    event_data = dict()
-    event_data.update({"project": heartbeat["project"], "filename": heartbeat["entity"], "language": heartbeat["language"], "time": heartbeat["time"]})
-    event = Event(timestamp=heartbeat["created_at"],duration=heartbeat["time"], data=event_data)
-    client.insert_event(bucket_id, event=event)
-    client.heartbeat(bucket_id, event=event, pulsetime=1,commit_interval=10)
+# increase this only if not all events are being sent
+sleep_after_events = 100
+count = 0
+
+for day in range(len(obj["days"])):
+    for heartbeat in obj["days"][day]["heartbeats"]:
+        heartbeat_data = dict()
+        datetime_created_at = datetime.datetime.strptime(heartbeat["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        heartbeat_data.update({"project": heartbeat["project"], "file": heartbeat["entity"], "language": heartbeat["language"]})
+        event = Event(timestamp=heartbeat["created_at"], data=heartbeat_data)
+        client.heartbeat(bucket_id, event=event, pulsetime=pulsetime, queued=True)
+        count +=1
+        if count % sleep_after_events == 0:
+            sleep(1)
